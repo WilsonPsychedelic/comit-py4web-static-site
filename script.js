79d55c3b8f8b7685b0fbf7b4189942d1d4bf6606
WilsonPsychedelic <78977926+WilsonPsychedelic@users.noreply.github.com>
2026-02-26 11:21:29 -0800

    Fix syntax error in button click event listener

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ const button = document.getElementById("magic-button");
 const box = document.getElementById("message-box");
 
 // 2. Listen for a click on the button
-button.addEventListener("click". function() {
+button.addEventListener("click", function() {
   // 3. When clicked, run this function:
   changeMessage();
 });
+
+// 4. Your magic function
+function changeMessage() {
+  box.textContent = "🎉 You clicked! I have been changed by JavaScript!";
+  box.classList.add("success-box");
+}
