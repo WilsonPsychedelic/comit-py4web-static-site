63766287f367746df8cdead959fcb82a986265e0
WilsonPsychedelic <78977926+WilsonPsychedelic@users.noreply.github.com>
2026-02-26 11:06:00 -0800

    Implement button click event to change message
    Add event listener to button for message change

---
+++ script.js
@@ -0,0 +1,9 @@
+// 1. Find the button and the div
+const button = document.getElementById("magic-button");
+const box = document.getElementById("message-box");
+
+// 2. Listen for a click on the button
+button.addEventListener("click". function() {
+  // 3. When clicked, run this function:
+  changeMessage();
+});
